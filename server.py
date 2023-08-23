6c122e5883c54eb1c4734c3afa408779237c5785
JulienGha <julien.ghali@efrei.net>
2023-08-24 00:07:06 +0900

    CORS

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from rq import Queue
 from worker import conn
 from tasks import process_image
 
 app = Flask(__name__)
+CORS(app)
 q = Queue(connection=conn)
 
 

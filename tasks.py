a43d75a9da51afff8df240d81d20fdbaa7d0419d
JulienGha <julien.ghali@efrei.net>
2023-08-23 22:00:41 +0900

    front updates

--- tasks.py
+++ tasks.py
@@ -3,14 +3,15 @@ import base64
 from PIL import Image
 from io import BytesIO
 
+
 def process_image(image_data_b64):
-    # Convert base64 image data to Image
+
     image_data = base64.b64decode(image_data_b64)
     image = Image.open(BytesIO(image_data))
     image_filename = f"temp_{os.urandom(4).hex()}.png"
     image.save(image_filename)
 
-    # Process with Tesseract
+    # Process with tesseract
     os.system(f"tesseract --psm 1 --oem 1 {image_filename} output")
     with open("output.txt", "r") as f:
         recognized_text = f.read().strip()
